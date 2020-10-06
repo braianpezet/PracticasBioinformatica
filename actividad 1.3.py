db9c18dca167cf122012980290037dbafe361867
braianpezet <braianpezet@gmail.com>
2020-10-06 17:41:05 -0300

    implemento fasta_to_array

--- actividad 1.3.py
+++ actividad 1.3.py
@@ -2,21 +2,36 @@
 #a dos proteínas del tipo Mioglobina (Myoglobin) para dos
 #especies diferentes (para dos animales distintos)
 
+from Bio import SeqIO
+import urllib
 import urllib.parse
 import requests
+import pypdb
+
+#devulve fasta en un array donde la pos 0 es el encabezado y la pos 1 es la secuencia 
+def fasta_to_array(archivo_fasta_url):
+   response = urllib.request.urlopen(archivo_fasta_url)
+   fasta_sequence = response.read().decode("utf-8", "ignore")
+   fasta_sequence = fasta_sequence.split('\n')
+   return (fasta_sequence)
 
 url = 'http://www.rcsb.org/pdb/rest/search'
 
 queryText = """
 
 <?xml version="1.0" encoding="UTF-8"?>
+
 <orgPdbQuery>
+
+<version>B0907</version>
+
 
 <queryType>org.pdb.query.simple.MoleculeNameQuery</queryType>
 
-<description>Molecule Name Search : Molecule Name=myoglobin</description>
+<description>Molecule Name Search : Molecule Name=mCherry</description>
 
-<macromoleculeName>myoglobin</macromoleculeName>
+<macromoleculeName>mCherry</macromoleculeName>
+
 
 </orgPdbQuery>
 
@@ -33,10 +48,21 @@ f = urllib.request.urlopen(req)
 
 
 result = f.read().decode("utf-8") 
-
+result = result.split()
 print(result)
+print(result[1][0:4]) #para quedarme con un id
+
+
 
 if result:
    print(("Found number of PDB entries:", result.count('\n')))
 else:
    print("Failed to retrieve results")
+
+url2 = "https://www.rcsb.org/fasta/entry/2C0K"
+
+
+
+#response = urllib.request.urlopen(url)
+#fasta = response.read().decode("utf-8", "ignore")
+#print (fasta)
